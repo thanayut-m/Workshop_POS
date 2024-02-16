80e630fb3b455809ee96d25f6494634c548c02b4
Thanayut Mungumklang <thanayut.mukl@gmail.com>
2024-02-16 23:18:33 +0700

    design By Tailwindcss

--- web/my-app/src/pages/package.js
+++ web/my-app/src/pages/package.js
@@ -1,61 +1,63 @@
-import { useState ,useEffect } from "react";
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 import config from "../config";
+import "../globals.css";
 
-function Package(){
-    const [Package ,setPackage] = useState([]);
+function Package() {
+  const [Package, setPackage] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        try {
-            axios.get(config.api_path +'/package/list').then(res =>{
-                setPackage(res.data.results)
-            }).catch(err => {
-                throw err.response.data;
-            })
-        } catch (e) {
-            console.log(e.message);
-        }
+  const fetchData = async () => {
+    try {
+      axios
+        .get(config.api_path + "/package/list")
+        .then((res) => {
+          setPackage(res.data.results);
+        })
+        .catch((err) => {
+          throw err.response.data;
+        });
+    } catch (e) {
+      console.log(e.message);
     }
+  };
 
-    return (
+  return (
+    <div>
+      <div className="text-center ">
         <div>
-            <div className="container mt-3">
-                <div className="h2 text-secondary">DainoPos : Point Of Sale on Clound</div>
-
-                <div className="h5">Package for You</div>
-                <div className="row">
-                    { Package.map((item, Index) =>
-                    <div key={Index} className="col-4">
-                        <div className="card">
-                            <div className="card-body text-center">
-                                <div className="h4 text-success">
-                                    {item.name}
-                                </div>
-                                <div className="h5">
-                                    {parseInt(item.bill_amount).toLocaleString('th-TH')}
-                                    &nbsp;
-                                    บิล/เดือน
-                                </div>
-                                <div className="h5 text-secondary">
-                                    {parseInt(item.price).toLocaleString('th-TH')}
-                                    &nbsp;
-                                    บาท/เดือน
-                                </div>
-                                <div className="mt-3">
-                                    <button className="btn btn-primary">สมัคร</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    )}
-                </div>
-            </div>
+          <p className="text-stone-500 font-bold text-[40px]">
+            DainoPos : Point Of Sale On Clound
+          </p>
         </div>
-    )
+        <div className="mt-3 text-lg text-[20px] font-bold">Package for You</div>
+
+        <div className=" mt-5 w-full flex flex-row items-center pb-10 md:px-10 lg:px-0 md:flex-row md:gap-5  justify-center ">
+          {Package.map((item, Index) => (
+            <div className="w-max bg-white md:w-60 lg:w-max ring-2 ring-yellow-500 rounded-xl md:p-2 lg:p-5 p-5 drop-shadow-md">
+              <div class="w-full flex flex-col justify-center items-center">
+                {item.name} 
+              </div>
+              <div className="mt-3 w-full flex flex-col justify-center items-center">
+                {parseInt(item.bill_amount).toLocaleString("th-TH")}&nbsp;บิล/เดือน
+              </div>
+              <div className="mt-3 w-full flex flex-col justify-center items-center">
+                {parseInt(item.price).toLocaleString("th-TH")}&nbsp;บาท/เดือน
+              </div>
+              <div className="mt-5">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    สมัครสมาชิก
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Package;
